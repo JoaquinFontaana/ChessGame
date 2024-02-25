c3bea439fd5ef53a4dc376da671f97c1de094d77
Joaquín Fontana <fontana200514@gmail.com>
2024-02-25 16:21:29 -0300

    Queen features

--- src/components/pieces/Queen.jsx
+++ src/components/pieces/Queen.jsx
@@ -1,9 +1,18 @@
 import PieceWhite from "../../assets/Piece=Queen, Side=White.png";
 import PieceBlack from "../../assets/Piece=Queen, Side=Black.png";
-
-export default function Queen({ team }) {
+import { useContext, useEffect } from "react";
+import { BoardContext } from "../../context/board";
+import useQueen from "./hooks/useQueen"
+export default function Queen({filaIndex, columnaIndex ,team }) {
   const src = team === "White" ? PieceWhite : PieceBlack;
-
+  
+  const{selectedPiece}=useContext(BoardContext)
+  const {showMovements}= useQueen(filaIndex,columnaIndex,team)
+  useEffect(()=>{
+    if(selectedPiece === `${filaIndex}-${columnaIndex}`){
+      showMovements()
+    }
+  },[selectedPiece])
   return (
     <span>
       <img src={src} alt="Queen" />
