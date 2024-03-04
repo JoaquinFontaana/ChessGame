635273c9015f3222a12c09d86749178ad797fba6
Joaquín Fontana <fontana200514@gmail.com>
2024-03-04 11:46:01 -0300

    Doc for useMakeSimulatedMove and optimize the copy of the pieces array

--- src/helpers/useMakeSimulatedMove.js
+++ src/helpers/useMakeSimulatedMove.js
@@ -4,29 +4,41 @@ import { PiecesContext } from '../context/pieces';
 
 export default function useMakeSimulatedMoves() {
     const { blackPieces, whitePieces, whiteKingPosition, blackKingPosition } = useContext(PiecesContext);
+    /**
+     * Simulates moves on a chess board and returns the legal moves.
+     *
+     * @param {Array} moves - The list of moves to simulate.
+     * @param {number} fromFilaIndex - The fila index of the piece to move.
+     * @param {number} fromColumnaIndex - The columna index of the piece to move.
+     * @param {Array} boardToSimulate - The chess board to simulate the moves on.
+     * @param {string} team - The team color of the piece to move.
+     * @returns {Array} - The legal moves after simulating the moves.
+     */
     function simulateMoves(moves, fromFilaIndex, fromColumnaIndex, boardToSimulate, team) {
         const { rookJaqueMoves, queenJaqueMoves, pawnJaqueMoves, bishopJaqueMoves, kingJaqueMoves, knightJaqueMoves, checkKingJaque } = checkJaque()
         const legalMoves = []
         console.log("moves to simulate", moves)
+        let blackPiecesCopy = []
+        let whitePiecesCopy = []
         moves.forEach(move => {
             const simulatedMove = boardToSimulate.map((fila) => fila.map((columna) => ({ ...columna })))
             const { fila, columna } = move
+  
+           simulatedMove[fila][columna] = {...simulatedMove[fromFilaIndex][fromColumnaIndex]} 
+           simulatedMove[fromFilaIndex][fromColumnaIndex] = { piece: undefined, team: undefined, classAdditional: undefined }
             /*
             //Si el movimiento es un ataque hacer una copia profunda de los whitePieces y blackPieces, 
             hacer un find para encontrar la pieza que se va a eliminar y eliminarla de la lista
             para poder iterar sobre las piezas restantes y verificar si el rey esta en jaque
             */
-           let blackPiecesCopy = blackPieces.map(piece => ({...piece}))
-           let whitePiecesCopy = whitePieces.map(piece => ({...piece}))
-            if (move.classAdditional === "attackable") {
                 if (team === "White") {
-                     blackPiecesCopy = blackPieces.filter(piece => !(piece.fila === fila && piece.columna === columna))
+                    if (move.classAdditional === "attackable") blackPiecesCopy = blackPieces.filter(piece => !(piece.fila === fila && piece.columna === columna))
+                    else blackPiecesCopy = blackPieces.map(piece => ({...piece}))
                 }
                 else {
-                     whitePiecesCopy = whitePieces.filter(piece => !(piece.fila === fila && piece.columna === columna))
-                }
-            }
-            simulatedMove[fromFilaIndex][fromColumnaIndex] = { piece: undefined, team: undefined, classAdditional: undefined }
+                     if(move.classAdditional === "attackable") whitePiecesCopy = whitePieces.filter(piece => !(piece.fila === fila && piece.columna === columna))
+                     else  whitePiecesCopy = whitePieces.map(piece => ({...piece}))
+                } 
             if (team === "White") {
                 blackPiecesCopy.forEach(piece => {
                     if (piece.piece === "King") {
